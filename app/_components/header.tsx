04db5555345deffa569e4b83bf232643c3aed9bd
renanaugust <renan.gabba@gmail.com>
2024-02-04 16:25:20 -0300

    Update logo and metadata

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -13,7 +13,7 @@ const Header = () => {
     <Card>
       <CardContent className="p-5 justify-between items-center flex flex-row">
         <Link href="/">
-          <Image src="/logo.png" alt="FSW Barber" height={18} width={120} />
+          <Image src="/logo_v2.png" alt="FSW Barber" height={18} width={120} />
         </Link>
         <Sheet>
           <SheetTrigger asChild>
